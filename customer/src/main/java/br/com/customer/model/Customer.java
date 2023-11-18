7cc8596fa2ed75dfe71867397ccfd3e47c4a272c
Mateus Cirino <mateuscirinosilva32@gmail.com>
2023-11-18 11:57:50 -0300

    adição dos métodos get/set da classe Customer

--- customer/src/main/java/br/com/customer/model/Customer.java
+++ customer/src/main/java/br/com/customer/model/Customer.java
@@ -21,4 +21,36 @@ public class Customer {
 
     @Column(nullable = false, unique = true)
     private String email;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDocument() {
+        return document;
+    }
+
+    public void setDocument(String document) {
+        this.document = document;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
