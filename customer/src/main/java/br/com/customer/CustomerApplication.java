487feebc04880c4f4575753471d605bdb18c32e2
Mateus Cirino <mateuscirinosilva32@gmail.com>
2023-11-18 19:54:03 -0300

    adicao de logs inicias

--- customer/src/main/java/br/com/customer/CustomerApplication.java
+++ customer/src/main/java/br/com/customer/CustomerApplication.java
@@ -1,13 +1,19 @@
 package br.com.customer;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CustomerApplication {
 
+	private static Logger LOGGER = LoggerFactory.getLogger(CustomerApplication.class);
+
 	public static void main(String[] args) {
+		LOGGER.info("Inicializando o micro serviço customer.");
 		SpringApplication.run(CustomerApplication.class, args);
+		LOGGER.info("Micro serviço customer inicializado com sucesso.");
 	}
 
 }
