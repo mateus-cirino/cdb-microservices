aa1662a169574600774c0901bdbb199f42ce219c
Mateus Cirino <mateuscirinosilva32@gmail.com>
2023-11-18 12:08:39 -0300

    adicao do projeto lombok e utilizacao do mesmo no Customer

--- customer/src/main/java/br/com/customer/model/Customer.java
+++ customer/src/main/java/br/com/customer/model/Customer.java
@@ -6,8 +6,12 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
 public class Customer {
     @Id
     @GeneratedValue(strategy= GenerationType.SEQUENCE)
@@ -21,36 +25,4 @@ public class Customer {
 
     @Column(nullable = false, unique = true)
     private String email;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDocument() {
-        return document;
-    }
-
-    public void setDocument(String document) {
-        this.document = document;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 }
