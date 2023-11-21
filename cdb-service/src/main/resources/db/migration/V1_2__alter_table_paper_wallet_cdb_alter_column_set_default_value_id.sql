ef3074d87cc3acd82182c3ad7a711c81f27b048e
Mateus Cirino <mateuscirinosilva32@gmail.com>
2023-11-21 16:52:58 -0300

    ajustes finos

--- cdb-service/src/main/resources/db/migration/V1_2__alter_table_paper_wallet_cdb_alter_column_set_default_value_id.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE paper ALTER COLUMN id SET DEFAULT nextval('hibernate_sequence');
-ALTER TABLE wallet_cdb ALTER COLUMN id SET DEFAULT nextval('hibernate_sequence');
-
--- customer/src/main/resources/db/migration/V1_2__alter_table_customer_alter_column_set_default_value_id.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE customer ALTER COLUMN id SET DEFAULT nextval('hibernate_sequence');
-
