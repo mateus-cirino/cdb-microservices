6c68727ee9bb459c024882322bd0dab064d896fb
Mateus Cirino <mateuscirinosilva32@gmail.com>
2023-11-21 11:03:44 -0300

    commit inicial cdb-service

---
+++ cdb-service/src/main/java/br/com/cdbservice/CdbServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.cdbservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdbServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CdbServiceApplication.class, args);
+	}
+
+}
