4224d8f9dcf54f23b7e5c829ad5d801962fb3171
Mateus Cirino <mateuscirinosilva32@gmail.com>
2023-11-23 10:13:27 -0300

    resolvido bug com o flyway que está misturando as versões 1.1 com a migration baseline

--- customer-wallet/src/main/resources/db/migration/V2_1__create_wallet_customer_table.sql
+++ customer-wallet/src/main/resources/db/migration/V2_1__create_wallet_customer_table.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS wallet_customer (
     id BIGINT PRIMARY KEY,
-    balance NUMERIC NOT NULL,
+    balance NUMERIC NOT NULL DEFAULT 0.0,
     customer_id BIGINT NOT NULL
 );
