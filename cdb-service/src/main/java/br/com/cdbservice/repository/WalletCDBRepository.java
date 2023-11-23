d2ee1a62ed3c8199bafe2c276eaac5f6a7596935
Mateus Cirino <mateuscirinosilva32@gmail.com>
2023-11-23 10:15:22 -0300

    adicao de uma query derivada que retorna todas as walletCDB de um determinado customer

--- cdb-service/src/main/java/br/com/cdbservice/repository/WalletCDBRepository.java
+++ cdb-service/src/main/java/br/com/cdbservice/repository/WalletCDBRepository.java
@@ -3,5 +3,8 @@ package br.com.cdbservice.repository;
 import br.com.cdbservice.model.entity.WalletCDB;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface WalletCDBRepository extends JpaRepository<WalletCDB, Long> {
+    List<WalletCDB> findAllByCustomerId(final Long customerId);
 }
